7d759e0fa4379673e70080c5f5e72afd611ae26e
Musab Gulfam <musabgulfam0722@gmail.com>
2025-02-05 02:07:04 +0500

    Core functionalities completed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -31,13 +30,6 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
--- components/Knob.tsx
+++ components/Knob.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Dimensions, Text } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming, useDerivedValue } from 'react-native-reanimated';
+import React from 'react';
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
+import Svg from 'react-native-svg';
+import Animated, { useAnimatedStyle, useSharedValue, runOnJS, useDerivedValue } from 'react-native-reanimated';
 import { cartesian2Canvas, ReText } from 'react-native-redash';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Appearance, useColorScheme } from 'react-native';
+import { useColorScheme } from 'react-native';
 import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import * as Haptics from 'expo-haptics';
 
 const BALL_SIZE = 50;
 
@@ -28,10 +28,11 @@ export default function Knob() {
 
     const progress = useSharedValue(0);
 
-    const translationX = useSharedValue(cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).x - 25);
-    const translationY = useSharedValue(cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).y - RADIUS - 25);
+    const translationX = useSharedValue(cartesian2Canvas({ x: -25, y: 0 }, { x: CENTER_X, y: CENTER_Y }).x);
+    const translationY = useSharedValue(cartesian2Canvas({ x: 0, y: RADIUS + 25 }, { x: CENTER_X, y: CENTER_Y }).y);
     const prevTranslationX = useSharedValue(cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).x);
     const prevTranslationY = useSharedValue(cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).y);
+    const prevTriggerredAngle = useSharedValue(0);
 
     const animatedStyles = useAnimatedStyle(() => ({
         transform: [
@@ -41,86 +42,102 @@ export default function Knob() {
     }));
 
     const progressText = useDerivedValue(() => {
-        return `${Math.floor(progress.value * 100)}`;
+        let _progress = Math.floor(progress.value)
+        return `${(_progress/10).toFixed(0)} ${_progress <= 1 ? 'minute' : 'minutes'}`;
     });
+
+    // Function to trigger haptics safely
+    const triggerHaptic = () => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    };
 
     const pan = Gesture.Pan()
         .minDistance(1)
-        // .failOffsetX([10, 10])
-        // .failOffsetY([10, 10])
         .shouldCancelWhenOutside(true)
         .onStart(() => {
             prevTranslationX.value = translationX.value;
             prevTranslationY.value = translationY.value;
         })
         .onUpdate((event) => {
-
             // Calculate new angle based on user movement
             const deltaX = event.translationX + prevTranslationX.value - CENTER_X;
             const deltaY = event.translationY + prevTranslationY.value - CENTER_Y;
+
             let angle = Math.atan2(deltaY, deltaX); // Calculate angle in radians
+            
+            // Adjust angle by +90 degrees to make 0° start from the top of the dial
+            let angleDeg = ((angle * 180) / Math.PI + 90 + 360) % 360;  // Normalize and adjust for top
+            
+            // Ensure angle is always positive (0° to 302°)
+            if (angleDeg > 302) return;
+            
+            progress.value = angleDeg;
 
-            // Convert angle to degrees and ensure it's positive
-            // let angleDeg = (angle * 180) / Math.PI;
-            progress.value = angle;
+            // Haptic/vibration feedback upon knob rotation of 20 degrees
+            if(Math.abs(angleDeg - prevTriggerredAngle.value) >= 20) {
+                runOnJS(triggerHaptic)(); // Ensure it's executed on the JS thread
+                prevTriggerredAngle.value = angleDeg
+            }
 
-            // Convert angle back to X, Y based on the circle's radius
             const newX = CENTER_X + RADIUS * Math.cos(angle);
             const newY = CENTER_Y + RADIUS * Math.sin(angle);
 
-            // Update translation values
             translationX.value = newX - 25;
             translationY.value = newY - 25;
-
-            // Normalize angle for progress (convert radians to percentage)
-            // progress.value = (angle) / (2 * Math.PI); // Normalize to 0-1 range
         })
 
     return (
         <GestureHandlerRootView>
-            <SafeAreaView style={styles.container}> 
+            <SafeAreaView style={styles.container}>
+                <TouchableOpacity
+                    onPress={() => {
+                        console.log(progress.value)
+                    }}
+                    style={{
+                        width: 200,
+                        height: 80,
+                        borderWidth: 3,
+                        borderColor: colorScheme === 'dark' ? 'white' : 'black',
+                        position: 'absolute',
+                        bottom: 70,
+                        right: CENTER_X - 200/2,
+                        padding: 20,
+                        zIndex: 3,
+                        borderRadius: 50
+                    }}
+                >
+                    <Text style={{
+                        color: colorScheme === 'dark' ? 'white' : 'black',
+                        fontSize: 20,
+                        textAlign: 'center'
+                    }}>Go</Text>
+                </TouchableOpacity>
                 {
                     ANGLES_DEGREES.map(angle => <View key={angle} style={{
                         position: 'absolute',
                         top:
-                            cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).y -
-                            RADIUS * Math.cos((angle * 10 + 2) * Math.PI / 180) + 20,
+                            cartesian2Canvas({ x: 0, y: RADIUS * Math.cos((angle * 10 + 2) * Math.PI / 180) - 16 }, { x: CENTER_X, y: CENTER_Y }).y,
                         left:
-                            cartesian2Canvas({ x: 0, y: 0 }, { x: CENTER_X, y: CENTER_Y }).x +
-                            RADIUS * Math.sin((angle * 10 + 2) * Math.PI / 180) - 10,
+                            cartesian2Canvas({ x: (RADIUS * Math.sin((angle * 10 + 2) * Math.PI / 180) - 6), y: 0 }, { x: CENTER_X, y: CENTER_Y }).x,
                         zIndex: 2
                     }}>
                         <Text style={{
-                            color: 'white',
+                            color: colorScheme === 'dark' ? 'white' : 'black',
                             textAlign: 'center'
                         }}>{angle}</Text>
                     </View>)
                 }
                 <ReText style={{
                     color: colorScheme === 'dark' ? 'white' : 'black',
-                    // textAlign: 'center',
                     fontSize: 30,
                     position: 'absolute',
                     top: CENTER_Y,
-                    left: CENTER_X - 35
+                    left: CENTER_X - 70,
+                    textAlign: 'center'
                 }} text={progressText} />
                 <GestureDetector gesture={pan}>
                     <Svg>
-                        <Circle
-                            cx={CENTER_X}
-                            cy={CENTER_Y}
-                            stroke={'#303858'}
-                            strokeWidth={50}
-                            r={RADIUS}
-                            fill={'none'}
-                        />
-                        <Animated.View style={[{
-                            borderColor: 'white',
-                            borderWidth: 3,
-                            width: 50,
-                            height: 50,
-                            borderRadius: 25,
-                        }, animatedStyles]} />
+                        <Animated.View style={[{...styles.ballStyle, borderColor: colorScheme === 'dark' ? 'white' : 'black'}, animatedStyles]} />
                     </Svg>
                 </GestureDetector>
             </SafeAreaView>
@@ -134,21 +151,12 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        // height: '100%',
     },
-    // ball: {
-    //     width: BALL_SIZE,
-    //     height: BALL_SIZE,
-    //     borderRadius: BALL_SIZE / 2,
-    //     // borderColor: 'black',
-    //     borderWidth: 1
-    //     // cursor: 'grab',
-    // },
-    // knob: {
-    //     // width: KNOB_DIAMETER,
-    //     // height: KNOB_DIAMETER,
-    //     borderRadius: RADIUS,
-    //     borderWidth: 3,
-    //     // borderColor: 'black',
-    // },
+    ballStyle: {
+        borderColor: 'white',
+        borderWidth: 3,
+        width: BALL_SIZE,
+        height: BALL_SIZE,
+        borderRadius: 25,
+    }
 });
